488da15a5a9fb9720be8d72ead6c0b262975592b
Renato Ferreira <renatoferreira.dev@gmail.com>
2025-02-10 19:19:15 +0100

    feat: update schema authorization

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,12 +3,17 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 const schema = a.schema({
   User: a.model({
     username: a.string()
-  }),
+  }).authorization((allow) => [
+    allow.publicApiKey().to(["read", "create", "update"]),
+  ]),
   Challenge: a.model({
     users: a.json(),
     result: a.json(),
     questions: a.json(),
-  }),
+  })
+  .authorization((allow) => [
+    allow.publicApiKey().to(["read", "create", "update"]),
+  ]),
   Phrase: a
     .model({
       text: a.string(),
